44ef602d1a26025b586ebf92d690a6b177d55cea
ViniciusLin <viniciuslinhares@visaojr.com.br>
2019-12-03 20:14:07 -0300

    Projeto

---
+++ func.js
@@ -0,0 +1,11 @@
+function calcularIMC(){
+	var formulario  = document.getElementById("formulario");
+
+	var kilos = formulario.kilos.value;
+	
+	var metros = formulario.metros.value;
+
+	var centimetros = formulario.centimetros.value;
+
+	alert(metros);
+}
